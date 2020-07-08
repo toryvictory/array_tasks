25fa3e93be59e536c428e46663dc45ddcf1f1d07
Victoria Rybalko <rybalko.victoria@gmail.com>
2020-07-08 21:16:14 +0300

    Add compare function as argument in method sort to compare numbers in array

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ console.log(`Reversed array: ${arr1.reverse()}`);
    3.  Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6.*/
 
 const arr3 = [1, 2, 3];
-arr3.push(4,5,6,);
+arr3.push(4, 5, 6,);
 console.log(`Array with pushed elements: ${arr3}`);
 
 
 /*4. Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.*/
 
 const arr4 = [1, 2, 3];
-arr4.unshift(4,5,6,);
+arr4.unshift(4, 5, 6,);
 
 console.log(`Shifted array: ${arr4}`);
 
@@ -50,48 +50,49 @@ console.log(`Last element of array: ${arr6.pop()}`);
     7. Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [1, 2, 3].*/
 const arr7 = [1, 2, 3, 4, 5];
 
-const arr8 = arr7.slice(0,3);
+const arr8 = arr7.slice(0, 3);
 console.log(`Sliced array: ${arr8}`);
 
 /* 8. Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [4, 5].*/
 
-const arr9 = arr7.slice(3,5);
+const arr9 = arr7.slice(3, 5);
 console.log(`Sliced array: ${arr9}`);
 
 /*Работа с splice
 Для решения задач данного блока вам понадобятся следующие методы: splice.
     9. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice преобразуйте массив в [1, 4, 5].*/
 
-arr7.splice(1,2);
+arr7.splice(1, 2);
 console.log(`Spliced array: ${arr7}`);
 
 /* 10. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice запишите в новый массив элементы [2, 3, 4].*/
 
 const arr10 = [1, 2, 3, 4, 5];
-arr10.splice(0,1);
-arr10.splice(3,1);
+arr10.splice(0, 1);
+arr10.splice(3, 1);
 console.log(`Spliced array: ${arr10}`);
 
 /*11. Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 2, 3, 'a', 'b', 'c', 4, 5].*/
 
 const arr11 = [1, 2, 3, 4, 5];
-arr11.splice(3, 0,'a', 'b', 'c' );
+arr11.splice(3, 0, 'a', 'b', 'c');
 console.log(`Spliced array: ${arr11}`);
 
 /*12.Дан массив [1, 2, 3, 4, 5]. С помощью метода splice сделайте из него массив [1, 'a', 'b', 2, 3, 4, 'c', 5, 'e'].*/
 
 const arr12 = [1, 2, 3, 4, 5];
-arr12.splice(1, 0,'a', 'b');
-arr12.splice(6, 0,'c');
-arr12.splice(8, 0,'e');
+arr12.splice(1, 0, 'a', 'b');
+arr12.splice(6, 0, 'c');
+arr12.splice(8, 0, 'e');
 console.log(`Spliced array: ${arr12}`);
 
 /*Работа с sort
 Для решения задач данного блока вам понадобятся следующие методы: sort.
     13. Дан массив [3, 4, 1, 2, 7]. Отсортируйте его.*/
 
-const arr13 = [3, 4, 1, 2, 7];
-console.log(`Sorted array: ${arr13.sort()}`);
+const arr13 = [30, 4, 1, 20, 7];
+arr13.sort((a, b) => a - b);
+console.log(`Sorted array: ${arr13}`);
 
 
 /*Работа с Object.keys
@@ -99,7 +100,7 @@ console.log(`Sorted array: ${arr13.sort()}`);
     14. Дан объект {js:'test', jq: 'hello', css: 'world'}. Получите массив его ключей.*/
 
 const obj1 = {
-    js:'test', jq: 'hello', css: 'world'
+    js: 'test', jq: 'hello', css: 'world'
 };
 console.log(`Array of object keys: ${Object.keys(obj1)}`);
 
